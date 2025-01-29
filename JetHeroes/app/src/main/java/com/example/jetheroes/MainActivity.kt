711c7f3524fd231b1bfcd10e8b59c3c928bb0bb5
Dimas Fahrul <dfpa.05@gmail.com>
2025-01-29 22:00:50 +0700

    feat: search and sticky column

---
+++ JetHeroes/app/src/main/java/com/example/jetheroes/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.jetheroes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.jetheroes.ui.theme.JetHeroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetHeroesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    JetHeroesApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HeroListItem(
+    name: String,
+    photoURL: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.clickable {}
+    ) {
+        AsyncImage(
+            model = photoURL,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroListItemPreview() {
+    JetHeroesTheme {
+        HeroListItem(
+            name = "H.O.S. Cockroaminoto",
+            photoURL = ""
+        )
+    }
+}
